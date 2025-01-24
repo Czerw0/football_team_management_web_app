f21a39209d746c6aa76d50f0cc6771c26b5e38bc
karol <karol.czerwo@gmail.com>
2025-01-24 18:05:34 +0100

    modele i capacity

--- Klub/Legia/admin.py
+++ Klub/Legia/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Team, Player, Assistant, Game, Coach
 # Register your models here.
+
+admin.site.register(Team)
+admin.site.register(Player)
+admin.site.register(Assistant)
+admin.site.register(Game)
+admin.site.register(Coach)
--- Klub/Legia/models.py
+++ Klub/Legia/models.py
@@ -1,26 +1,28 @@
 from django.db import models
 import calendar
+from django.core.exceptions import ValidationError
 
 # Player, Team, Coach, Assistant, Game 
 GAMES_SPOTS = (
-    ('Wyjazd', 'Na wyjęździe'),
-    ('Dom', "U siebie")
+    ('Na wyjeździe', 'Na wyjeździe'),
+    ('U siebie', "U siebie")
 )
 
 GAMES_TYPES = (
     ('Liga', 'Liga'),
-    ('Puchar_Kraju', "Puchar Kraju"),
-    ('CL', "Champions League"),
-    ('EL', 'Europa League')
+    ('Puchar Kraju', "Puchar Kraju"),
+    ('Champions League', "Champions League"),
+    ('Europa League', 'Europa League'),
+    ('Spotkanie Towarzyskie', 'Spotkanie Towarzyskie'),
 )
 
 SPECIALIZATIONS = (
-        ('BR_trener', 'Trener Bramkarzy'),
-        ('O_trener', 'Trener Obrońców'),
-        ('P_trener', 'Trener Pomocników'),
-        ('N_trener', 'Trener Napastników'),
-        ('Taktyk', 'Taktyk')
-        ('Trener_motoryczny','Trener Motoryczny')
+        ('Trener Bramkarzy', 'Trener Bramkarzy'),
+        ('Trener Obrońców', 'Trener Obrońców'),
+        ('Trener Pomocników', 'Trener Pomocników'),
+        ('Trener Napastników', 'Trener Napastników'),
+        ('Taktyk', 'Taktyk'),
+        ('Trener Motoryczny', 'Trener Motoryczny'),
     )
 
 POSITIONS = (
@@ -30,7 +32,7 @@ POSITIONS = (
         ('PO', 'Prawy Obrońca'),
         ('LS', 'Lewy Skrzydłowy'),
         ('PS', 'Prawy Skrzydłowy'),
-        ('OP', 'Ofensywny pomoPomocnikcnik'),
+        ('OP', 'Ofensywny Pomocnik'),
         ('DP', 'Defensywny Pomocnik'),
         ('ŚP', 'Środkowy Pomocnik'),
         ('N', 'Napastnik'),
@@ -38,8 +40,10 @@ POSITIONS = (
         ('PN', 'Prawy Napastnik'),
 )
 TEAMS = (
-    ('main', 'Wyjściowa 11')
-    ('rez', 'Rezerwa')
+    ('Wyjściowa 11', 'Wyjściowa 11'),
+    ('Rezerwa', 'Rezerwa'),
+    ('Druga Drużyna', 'Druga Dużyna'),
+    ('Akademia Juniorska', 'Akademia Juniorska')
 )
 
 PLEC = (
@@ -49,31 +53,46 @@ PLEC = (
 )
 
 class Team(models.Model):
-    name = models.CharField(max_length=1, choices=TEAMS)
+    name = models.CharField(max_length=100, choices=TEAMS)
+    capacity = models.PositiveBigIntegerField(null = True)  # Maksymalna liczba zawodników w drużynie
 
     def __str__(self):
         return f"{self.name}"
-    
+
+    def current_player_count(self):
+        """Zwraca aktualną liczbę zawodników w drużynie."""
+        return self.player_set.count()
+
+
 class Player(models.Model):
     firstname = models.CharField(max_length=50)
     lastname = models.CharField(max_length=50)
-    age = models.DateField()
-    plec = models.CharField(max_length=1, choices=PLEC, null = True)
-    position = models.CharField(max_length=1, choices=POSITIONS)
+    birth = models.DateField()
+    plec = models.CharField(max_length=100, choices=PLEC, null=True)
+    position = models.CharField(max_length=100, choices=POSITIONS)
     number = models.PositiveBigIntegerField()
     signing_date = models.DateTimeField(auto_now_add=True)
     team = models.ForeignKey(Team, null=True, blank=True, on_delete=models.SET_NULL)
 
-
     def __str__(self):
-        return f'{self.firstname} {self.lastname} ; {self.position}'
+        return f'{self.firstname} {self.lastname} ; {self.position} ; {self.team}'
+
+    class Meta:
+        ordering = ['team','position']
+
+    def save(self, *args, **kwargs):
+        """Sprawdź, czy drużyna ma miejsce przed zapisaniem zawodnika."""
+        if self.team:
+            if self.team.current_player_count() >= self.team.capacity:
+                raise ValidationError(f"Drużyna {self.team.name} osiągnęła maksymalną liczbę zawodników ({self.team.capacity}).")
+        super().save(*args, **kwargs)
     
 
 class Coach(models.Model):
     firstname = models.CharField(max_length=50)
     lastname = models.CharField(max_length=50)
-    age = models.DateField()
-    plec = models.CharField(max_length=1, choices=PLEC, null = True)
+    birth = models.DateField()
+    plec = models.CharField(max_length=100, choices=PLEC, null = True)
     signing_date = models.DateTimeField(auto_now_add=True)
     team = models.ForeignKey(Team, null=True, blank=True, on_delete=models.SET_NULL)
 
@@ -83,19 +102,29 @@ class Coach(models.Model):
 class Assistant(models.Model):
     firstname = models.CharField(max_length=50)
     lastname = models.CharField(max_length=50)
-    age = models.DateField()
-    plec = models.CharField(max_length=1, choices=PLEC, null = True)
+    birth = models.DateField()
+    plec = models.CharField(max_length=100, choices=PLEC, null = True)
     signing_date = models.DateTimeField(auto_now_add=True)
-    specialization = models.CharField(max_length=50)
+    specialization = models.CharField(max_length=50, choices=SPECIALIZATIONS)
     team = models.ForeignKey(Team, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
-        return f'{self.firstname} {self.lastname} ; specjalizacja = {self.specialization}'
+        return f'{self.firstname} {self.lastname} ; {self.specialization} ; {self.team}'
     
-class Games(models.Model):
+    class Meta:
+        ordering = ['lastname']
+
+class Game(models.Model):
     opponent = models.CharField(max_length=50)
     team = models.ForeignKey(Team, null=True, blank=True, on_delete=models.SET_NULL)
-    game_spot = models.IntegerField(choices=GAMES_SPOTS.choices)
-    game_type = models.IntegerField(choices=GAMES_TYPES.choices)
-    score = models.CharField(max_length=50)
+    game_spot = models.CharField(max_length=50, choices=GAMES_SPOTS, null = True)
+    game_type = models.CharField(max_length=50, choices=GAMES_TYPES, null = True)
+    date = models.DateField()
+    score_legia = models.CharField(max_length=50,null = True)
+    score_opponent = models.CharField(max_length=50,null = True)
+
+    def __str__(self):
+        return f'Legia Warszawa     {self.score_legia} - {self.score_opponent}     {self.opponent} ; {self.date}'
     
+    class Meta:
+        ordering = ['date']
